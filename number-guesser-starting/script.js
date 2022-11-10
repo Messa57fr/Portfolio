5da9d093b1863fa58bfee750182c562cd088e69a
Samuel Coizet <messa57fr@gmail.com>
2022-11-10 15:45:59 +0100

    CA js

--- number-guesser-starting/script.js
+++ number-guesser-starting/script.js
@@ -4,3 +4,16 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+const generateTarget = () =>
+  Math.floor(Math.random() * 10) + 1
+
+const compareGuesses = (userGuess, computerGuess, secretTargetNumber) => {
+  if (Math.abs(userGuess - secretTargetNumber) <= Math.abs(computerGuess - secretTargetNumber))
+    return "User wins";
+  else
+    return "Computer wins";
+}
+
+
+  console.log(generateTarget())
+  console.log(compareGuesses(3,5,4))
