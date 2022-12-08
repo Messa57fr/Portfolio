ded5dcae8f26056a022e5123eef451ee61f00f4f
Samuel Coizet <messa57fr@gmail.com>
2022-12-08 20:14:41 +0100

    bug to fix

--- portfolio/src/components/Home/index.js
+++ portfolio/src/components/Home/index.js
@@ -1,23 +1,63 @@
 import "./index.scss";
 import SLogo from "../../assets/images/S-Logo.png";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import AnimatedLetters from "../AnimatedLetters";
+import Logo from "../Logo";
 
 const Home = () => {
+  const [letterClass, setLetterClass] = useState("text-animate");
+  const nameArray = ["a", "m"];
+  const jobArray = [
+    "w",
+    "e",
+    "b",
+    " ",
+    "d",
+    "e",
+    "v",
+    "e",
+    "l",
+    "o",
+    "p",
+    "e",
+    "r",
+  ];
+
+  useEffect(() => {
+    setTimeout(() => {
+      return setLetterClass("text-animate-hover");
+    }, 4000);
+  }, []);
+
   return (
     <div className="container home-page">
       <div className="text-zone">
         <h1>
-          {" "}
-          Hi, <br /> I'm
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
+          <br />
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m</span>
           <img src={SLogo} alt="developer" />
-          am <br />
-          Web Developer
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
+          />
+          <br />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArray}
+            idx={17}
+          />
         </h1>
-        <h2>Ruby / Next.js / React / Js / Css / Postgre</h2>
+        <h2>Ruby / Next / Js / React / Css / PostgreSQL</h2>
         <Link to="/contact" className="flat-button">
           Contact Me
         </Link>
       </div>
+      <Logo />
     </div>
   );
 };
